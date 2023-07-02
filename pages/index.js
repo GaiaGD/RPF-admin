ad584aa81faf492f7cc53b11d672ec9aca6040e7
GaiaGD <gaia.digregorio@gmail.com>
2023-07-01 23:07:13 -0400

    clean

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,22 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
   const { data: session } = useSession()
 
-  if (!session) {
-    return (
-      <div className={`bg-emerald-900 text-emerald-900 w-screen h-screen flex items-center`}>
-        <div className={'w-full text-center'}>
-          <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-md'>Login with Google</button>
-        </div>
-      </div>
-    )
-  }
 
   return (
-    <>
-      <div>Logged in:</div>
-      <div>{session.user.email}</div>
-    </>
+      <div className={`bg-emerald-900 w-screen h-screen flex items-center`}>
+        <div className={'w-full text-center'}>
+          {session ?
+            <>
+              <div className="p-2">
+                <h3>Logged in:</h3>
+                <h3>{session.user.email}</h3>
+              </div>
+              <button onClick={() => signOut()} className='bg-white text-emerald-900 p-2 px-4 rounded-md'>Sign out</button>
+            </>
+            :
+            <button onClick={() => signIn('google')} className='bg-white text-emerald-900 p-2 px-4 rounded-md'>Login with Google</button>
+          }
+        </div>
+      </div>
   )
-
 }
