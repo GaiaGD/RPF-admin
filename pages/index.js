9201186df363ff4b46b07b8b00cab37e6bc21f16
GaiaGD <gaia.digregorio@gmail.com>
2023-07-02 00:27:06 -0400

    dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "./components/Nav"
 
 export default function Home() {
   const { data: session } = useSession()
 
 
   return (
-      <div className={`bg-emerald-900 w-screen h-screen flex items-center`}>
-        <div className={'w-full text-center'}>
+      <div className={`bg-emerald-900 w-screen min-h-screen`}>
           {session ?
-            <>
-              <div className="p-2">
-                <h3>Logged in:</h3>
-                <h3>{session.user.email}</h3>
+            <div className="flex h-screen">
+              <Nav />
+              <div className="p-4 bg-white flex-grow text-emerald-900">
+                <div>
+                  <h3>Logged in:</h3>
+                  <h3>{session.user.email}</h3>
+                </div>
+                <button onClick={() => signOut()} className='text-white bg-emerald-900 p-2 px-4 rounded-md'>Sign out</button>
               </div>
-              <button onClick={() => signOut()} className='bg-white text-emerald-900 p-2 px-4 rounded-md'>Sign out</button>
-            </>
+            </div>
             :
-            <button onClick={() => signIn('google')} className='bg-white text-emerald-900 p-2 px-4 rounded-md'>Login with Google</button>
+            <div className={"flex justify-center items-center h-screen"}>
+              <button onClick={() => signIn('google')} className='bg-white text-emerald-900 p-2 px-4 rounded-md'>Login with Google</button>
+            </div>
           }
-        </div>
       </div>
   )
 }
