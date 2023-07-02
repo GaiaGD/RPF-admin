683ad33bebf42669ebb7b1015f888a8e9371d069
GaiaGD <gaia.digregorio@gmail.com>
2023-07-01 23:33:31 -0400

    mongo setup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
@@ -23,5 +25,6 @@ export default NextAuth({
     //   server: process.env.MAIL_SERVER,
     //   from: 'NextAuth.js <no-reply@example.com>'
     // }),
-  ]
+  ],
+  adapter: MongoDBAdapter(clientPromise),
 })
