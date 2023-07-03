a59ceb930122c3e9872995a2cdfc0f97c6bb984d
GaiaGD <gaia.digregorio@gmail.com>
2023-07-02 21:50:49 -0400

    image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import { useSession } from "next-auth/react"
 import Layout from "./components/Layout"
-
+import Image
+ from "next/image"
 export default function Home() {
+  const { data: session} = useSession()
   return(
-    <Layout>test</Layout>
+    <Layout>
+      <div className="text-emerald-900">
+        <h3>Hello {session?.user?.email}</h3>
+        <img src={session?.user?.image} />
+      </div>
+    </Layout>
   )
 }
