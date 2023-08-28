1284fcbf68c4d9860788507a5be97ff0f203ed45
GaiaGD <gaia.digregorio@gmail.com>
2023-08-28 19:01:30 -0400

    orders page

--- pages/orders.js
+++ pages/orders.js
@@ -1,9 +1,52 @@
+import { useEffect, useState } from "react";
 import Layout from "./components/Layout";
+import axios from "axios";
+import { nanoid } from 'nanoid'
+
 
 export default function Orders(){
+    const [orders, setOrders] = useState([])
+    useEffect(() => {
+        axios.get('api/orders').then(response => {
+            setOrders(response.data)
+        })
+        console.log(orders)
+    }, [])
+
     return (
         <Layout>
             <h1 className="font-bold page-names">Orders</h1>
+
+            <table className="border w-full mt-5">
+                    <thead className="border bg-green-100">
+                        <tr>
+                            <td className="p-2">Date</td>
+                            <td className="p-2">Recepient</td>
+                            <td className="p-2">Products</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.length > 0 && orders.map(ord => (
+                            <tr key={nanoid()}>
+                                <td>{(new Date(ord.createdAt)).toLocaleString()}</td>
+                                <td>
+                                    {ord.firstname} {ord.lastname}<br/>
+                                    {ord.email}<br/>
+                                    {ord.address}<br/>
+                                    {ord.city} {ord.postcode}<br/>
+                                    {ord.state}, {ord.country}
+                                </td>
+                                <td>
+                                    {ord.line_items.map(l => (
+                                        <>{l.price_data?.product_data.name} x {l.quantity} <br/>
+                                        </>
+                                    ))}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+            </table>
+
         </Layout>
     )
 }
